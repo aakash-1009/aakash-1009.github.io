dcd0d476aa49b5d4727b34a08f294b09f301e1df
aakash garg <aakashgarg10101999@gmail.com>
2021-02-24 15:07:13 +0530

    FRONTEND COMPLETED 100% RESPONSIVE

--- js/main.js
+++ js/main.js
@@ -7,6 +7,18 @@ open.addEventListener("click", () => header.classList.add("show-nav"));
 close.addEventListener("click", () => header.classList.remove("show-nav"));
 
 // -----------------nav button ends--------------------
+// mobile nav--------------------
+const open_btn = document.querySelector('.open-btn')
+const close_btn = document.querySelector('.close-btn')
+const nav = document.querySelectorAll('.nav')
+
+open_btn.addEventListener('click', () => {
+    nav.forEach(nav_el => nav_el.classList.add('visible'))
+})
+
+close_btn.addEventListener('click', () => {
+    nav.forEach(nav_el => nav_el.classList.remove('visible'))
+})
 // ---------------typing animation-------------------
 var typed = new Typed(".typing", {
   strings: ["Developer", "Student", "Designer", "Freelancer"],
